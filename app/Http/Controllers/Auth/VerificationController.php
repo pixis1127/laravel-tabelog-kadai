69261dc8c46f013710abf16045ff6ececa2bcf54
pixis1127 <yumi.kawasaki1127@gmail.com>
2024-08-04 19:13:25 +0900

    8.4最終課題の更新

--- app/Http/Controllers/Auth/VerificationController.php
+++ app/Http/Controllers/Auth/VerificationController.php
@@ -25,7 +25,7 @@ class VerificationController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/stores';
 
     /**
      * Create a new controller instance.
