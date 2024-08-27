8901cc7792ef2e1b973e2928b190c949eddebe0b
pixis1127 <yumi.kawasaki1127@gmail.com>
2024-08-27 23:18:04 +0900

    最終課題の更新

--- resources/views/users/reservation.blade.php
+++ resources/views/users/reservation.blade.php
@@ -12,14 +12,13 @@
                  <div class="col-md-7 mt-2">
                      <div class="d-inline-flex">
                          <a href="{{ route('stores.show', $reservation->id) }}" class="w-25">
-                         <a href="{{ route('mypage.reservation', $reservation->id) }}" class="w-25">
                              <img src="{{ asset('img/dummy.png')}}" class="img-fluid w-100">
                          </a>
                          <div class="container mt-3">
                              <h5 class="w-100 kadai_002-reservation-item-text">{{ $reservation->store->name }}</h5>
                              <h6 class="w-100 kadai_002-reservation-item-text">&yen;{{ $reservation->store->price }}</h6>
-                             <h7 class="w-100 kadai_002-reservation-item-text">{{ $reservation->reservation_day }}</h7>
-                             <h8 class="w-100 kadai_002-reservation-item-text">{{ $reservation->reservation_time }}</h8>
+                             <h7 class="w-100 kadai_002-reservation-item-text">{{ $reservation->reservation_day->reservation_day }}</h7>
+                             <h8 class="w-100 kadai_002-reservation-item-text">{{ $reservation->reservation_time->reservation_time }}</h8>
                          </div>
                      </div>
                  </div>
